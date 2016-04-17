dc4e291086e9b6f4cb82ee5cbc762c643e90359d
cryptolemming <aliayoub22@gmail.com>
2016-04-17 14:14:28 -0400

    removed insecure and allowed certain writes to posts

--- microscope/lib/collections/posts.js
+++ microscope/lib/collections/posts.js
@@ -1,2 +1,8 @@
 // no 'var' so to be global scope, otherwise local to file only
 Posts = new Mongo.Collection('posts');
+
+Posts.allow({
+	insert: function(userId, doc) {
+		return !! userId;
+	}
+});
