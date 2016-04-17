7a8bd731155a9aa23d56d15c20bfd04ee5f87263
cryptolemming <aliayoub22@gmail.com>
2016-04-17 14:27:05 -0400

    Use a method to submit the post

--- microscope/lib/collections/posts.js
+++ microscope/lib/collections/posts.js
@@ -1,8 +1,25 @@
-// no 'var' so to be global scope, otherwise local to file only
 Posts = new Mongo.Collection('posts');
 
-Posts.allow({
-	insert: function(userId, doc) {
-		return !! userId;
-	}
+Meteor.methods({
+ postInsert: function(postAttributes) {
+ 	check(Meteor.userId(), String);
+ 	check(postAttributes, {
+ 		title: String,
+ 		url: String
+ 	});
+
+ 	var user = Meteor.user();
+
+ 	var post = _.extend(postAttributes, {
+ 		userId: user._id,
+ 		author: user.username,
+ 		submitted: new Date()
+ 	});
+
+ 	var postId = Posts.insert(post);
+
+ 	return {
+ 		_id: postId
+ 	};
+ }
 });
