f896e85dd1a1cf5eb92fb87e6994d7589794d51c
cryptolemming <aliayoub22@gmail.com>
2016-04-19 15:48:08 -0400

    Added edit posts form

--- microscope/lib/router.js
+++ microscope/lib/router.js
@@ -4,12 +4,21 @@ Router.configure({
  notFoundTemplate: 'notFound',
  waitOn: function() { return Meteor.subscribe('posts'); }
 });
+
 Router.route('/', {name: 'postsList'});
+
 Router.route('/posts/:_id', {
  name: 'postPage',
  data: function() { return Posts.findOne(this.params._id); }
 });
+
+Router.route('/posts/:_id/edit', {
+	name: 'postEdit',
+	data: function() { return Posts.findOne(this.params._id);}
+})
+
 Router.route('/submit', {name: 'postSubmit'});
+
 var requireLogin = function() {
  if (! Meteor.user()) {
  if (Meteor.loggingIn()) {
@@ -21,5 +30,6 @@ var requireLogin = function() {
  this.next();
  }
 }
+
 Router.onBeforeAction('dataNotFound', {only: 'postPage'});
 Router.onBeforeAction(requireLogin, {only: 'postSubmit'});
