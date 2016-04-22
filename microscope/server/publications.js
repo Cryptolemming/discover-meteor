84d13b1865bb36febaf9b8142dcc7d16ed70c668
cryptolemming <aliayoub22@gmail.com>
2016-04-22 14:27:46 -0400

    Use a single post subscription to ensure that we can always see the right post

--- microscope/server/publications.js
+++ microscope/server/publications.js
@@ -1,9 +1,10 @@
 Meteor.publish('posts', function() {
-	check(options, {
-		sort: Object,
-		limit: Number
-	});
 	return Posts.find({}, options);
+});
+
+Meteor.publish('singlePost', function() {
+	check(id, String)
+	return Posts.find(id);
 });
 
 Meteor.publish('comments', function(postId) {
